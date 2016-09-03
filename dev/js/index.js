2521dbb6f9086ba2f14f57236700b5db64329c7d
rodk <rod2010rod2010@hotmail.com>
2016-09-02 21:03:20 -0400

    corrected 'bundle.js' correct path

--- dev/js/index.js
+++ dev/js/index.js
@@ -1,22 +1,22 @@
 import 'babel-polyfill'  
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {Provider} from 'react-redux'
-// import {createStore, applyMiddleware} from 'redux'
-import {createStore} from 'redux'
-// import thunk from 'redux-thunk'
-// import promise from 'react-promise'
-// import createLogger from 'create-logger'
-import allReducers from './reducers'; 
+//import {Provider} from 'react-redux'
+// // import {createStore, applyMiddleware} from 'redux'
+// import {createStore} from 'redux'
+// // import thunk from 'redux-thunk'
+// // import promise from 'react-promise'
+// // import createLogger from 'create-logger'
+// import allReducers from './reducers'; 
 import App from './components/app'
 
 //const logger = createLogger();
-const store = createStore(allReducers);
+// const store = createStore(allReducers);
 
 ReactDOM.render(
-<Provider store={store}>
+//<Provider>
     <App />,
-</Provider>, 
+//</Provider>, 
   document.getElementById('root')
 );
 
