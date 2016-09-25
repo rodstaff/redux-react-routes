b176d5206ecb4242ff9aad49a66a048d3cf52535
rodk <rod2010rod2010@hotmail.com>
2016-09-25 13:20:49 -0400

    modified view

--- routes/home.js
+++ routes/home.js
@@ -12,6 +12,6 @@ export const Home = () => (
   <div>
     <br />
     <h1>Welcome to the World of Pharaohs and Turtles!</h1>
-    
+    <h4>Click on the above links to know more...</h4>
   </div>
 )
