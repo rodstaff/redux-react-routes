8ff373e33489f8fdd138be7dad50ebc43308876b
rodk <rod2010rod2010@hotmail.com>
2016-09-28 22:34:01 -0400

    returned routes/indes.js file

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Provider} from 'react-redux'
+
+import App from '../routes/app.js'
+import store from '../dev/js/store'
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
