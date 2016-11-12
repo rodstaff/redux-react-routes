a160e7191378da02cc5f4e3aead7d6ca6279a3e9
rodk <rod2010rod2010@hotmail.com>
2016-11-11 19:09:47 -0500

    copied repo to 'famu-tant'

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
-  //entry: './dev/js', 
+  //entry: './dev/js',,
   entry: './routes', 
   output: {
     path: '../rr-heroku/',
